05673839f1bf6d7a2bbede3c65ed063e13489e22
anneddias <annelisediaso@gmail.com>
2025-06-10 19:17:59 -0300

    Create opportunityPipeline.js

---
+++ automations/sales-pipeline-automation/opportunityPipeline.js
@@ -0,0 +1,6 @@
+columns = [
+    { label: 'Nome da Oportunidade', fieldName: 'Name' },
+    { label: 'Valor', fieldName: 'Amount', type: 'currency' },
+    { label: 'Fase', fieldName: 'StageName' },
+    { label: 'Data de Fechamento', fieldName: 'CloseDate', type: 'date' }
+];
